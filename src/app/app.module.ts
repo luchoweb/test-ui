59cfac785d0d0034da0653abfba7ec80f4eb6b41
Luis Rodriguez <webfacilco@gmail.com>
2019-11-23 18:08:03 -0500

    reorder components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { NavbarComponent } from './components/shared/header/navbar/navbar.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
 import { PollHeaderComponent } from './components/poll-header/poll-header.component';
 import { PreviousRulingComponent } from './components/previous-ruling/previous-ruling.component';
-import { SubmitNameComponent } from './components/footer/submit-name/submit-name.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { SubmitNameComponent } from './components/shared/footer/submit-name/submit-name.component';
 
 @NgModule({
   declarations: [
