2a75eb80fe4a3ae69e07eaaa1436b64e63562129
Lucho Web <webfacil.co@gmail.com>
2019-11-25 20:30:05 -0500

    Dynamic bars with percents in previuos rulings and others

--- src/app/components/previous-ruling/previous-ruling.component.ts
+++ src/app/components/previous-ruling/previous-ruling.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { AppServices } from '../../services/app-services.service';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-previous-ruling',
   templateUrl: './previous-ruling.component.html',
-  styleUrls: ['./previous-ruling.component.scss']
+  styleUrls: ['./previous-ruling.component.scss'],
+  providers:  [ AppServices ]
 })
 export class PreviousRulingComponent implements OnInit {
 
-  constructor() { }
+  public polls: any = '';
+
+  constructor( private appServices: AppServices) {}
 
   ngOnInit() {
+    this.getData();
   }
 
+  getData() {
+    this.appServices.getJSON()
+      .subscribe(resp => {
+        this.polls = resp.filter( ( poll ) => poll.type === 'normal' );
+      });
+  }
+
+  percentBars( type, vUp, vDown ) {
+    let percent = 0;
+    if ( type === 'up' ) {
+      percent = (vUp * 100) / (vUp + vDown);
+    } else if ( type === 'down' ) {
+      percent = (vDown * 100) / (vUp + vDown);
+    }
+
+    return percent.toFixed(0);
+  }
+
+  formatDate( date ) {
+    return moment(date, 'YYYY-MM-DD HH:mm').fromNow();
+  }
 }
